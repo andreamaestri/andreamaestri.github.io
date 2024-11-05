f889e03274084c93529680d311cf841745cb53a9
Andrea <16215183+andreamaestri@users.noreply.github.com>
2024-11-05 14:39:26 +0000

    Refactor date formatting, enhance header script, add gallery component, and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
+import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
+// https://astro.build/config
 export default defineConfig({
   site: "https://andreamaestri.github.io",
   integrations: [mdx(), sitemap(), tailwind(), icon()],
+  experimental: {
+    contentLayer: true,
+  },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,13 @@
 import { defineCollection, z } from "astro:content";
+import { cldAssetsLoader } from "astro-cloudinary/loaders";
+
+// Define the collections
+const assets = defineCollection({
+  loader: cldAssetsLoader({
+    limit: 1200,
+    folder: "2024", // Replace with your folder name
+  }),
+});
 
 const blog = defineCollection({
   type: "content",
@@ -6,7 +15,7 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
-    draft: z.boolean().optional()
+    draft: z.boolean().optional(),
   }),
 });
 
@@ -22,14 +31,13 @@ const work = defineCollection({
 
 const projects = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-    demoURL: z.string().optional(),
-    repoURL: z.string().optional()
-  }),
+  // Include the schema and other configurations as needed
 });
 
-export const collections = { blog, work, projects };
+// Export all collections in the `collections` object
+export const collections = {
+  assets,
+  blog,
+  work,
+  projects,
+};
