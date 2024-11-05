0362335d829161b8093ab60c2b41f04e50140cca
Andrea <16215183+andreamaestri@users.noreply.github.com>
2024-11-05 15:33:20 +0000

    Refactor Header component and global CSS for improved layout and responsiveness

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
+  darkMode: ["class", '[data-theme="dark"]'],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
@@ -37,6 +37,19 @@ export default {
           800: '#525477',
           900: '#464961',
           950: '#292b3b',
+        },
+        zinc: {
+          50: '#fafafa',
+          100: '#f4f4f5',
+          200: '#e4e4e7',
+          300: '#d4d4d8',
+          400: '#a1a1aa',
+          500: '#71717a',
+          600: '#52525b',
+          700: '#3f3f46',
+          800: '#27272a',
+          900: '#18181b',
+          950: '#0f0f10',
         },
       },
       animation: {
